bcdf86238acaa68692d0059b2ec58b1d2329cb59
P03W <52360088+P03W@users.noreply.github.com>
2020-04-26 01:40:03 +0000

    Who could have guessed, its more initial commit

---
+++ src/main/java/mc/lavafishing/mixin/ItemHealth.java
@@ -0,0 +1,21 @@
+package mc.lavafishing.mixin;
+
+import net.minecraft.entity.ItemEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ItemEntity.class)
+public class ItemHealth {
+    @Shadow
+    private int health;
+    @Inject(at = @At("RETURN"), method = "method_24348()V")
+    public void manipHealth(CallbackInfo ci) {
+        ItemEntity ie = (ItemEntity) (Object) this;
+        if (ie.world.dimension.isNether()) {
+            health *= 250;
+        }
+    }
+}
